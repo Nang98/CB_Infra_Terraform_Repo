5cfbb281869148d6495cb4a8c0fa8d52bfa8117e
Nang98 <65604815+Nang98@users.noreply.github.com>
2025-06-26 22:31:10 +0630

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "code_version" {
+  description = "The Git tag or version of the IaC code"
+  type        = string
+}
+
 variable "availability_zones" {
   type = list(string)
   default = ["us-east-1a", "us-east-1b"] 
